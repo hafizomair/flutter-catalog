8ab67c73486f3b922dde0b86a9183169a1f3e487
hafizomair <omair.bcs@gmail.com>
2022-05-31 02:04:41 +0500

    day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/pages/home_page.dart';
 
 void main() {
   runApp(myApp());
@@ -10,13 +11,7 @@ class myApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Welcom my 1st app"),
-          ),
-        ),
-      ),
+      home: Homepage(),
     );
   }
 }
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatelessWidget {
+  const Homepage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcom my 1st app"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
